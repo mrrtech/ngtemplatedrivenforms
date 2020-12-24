3163ce85082f17ad750910a83cface7d336139a1
Ramesh <ramesh.mavuluri.info@gmail.com>
2020-12-24 16:15:57 -0500

    prettier added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'space-ng-templateforms';
+  title = 'space-ng-templateforms'
+  model: any = {}
+  onSubmit() {
+    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.model))
+  }
 }
